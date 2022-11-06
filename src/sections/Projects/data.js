1e89f1e752632d9c27dabd1d9ab680ef247a949b
Prathmesh Santosh Dhatrak <prathmesh101dhatrak@gmail.com>
2022-11-06 15:58:32 +0530

    error fix

--- src/sections/Projects/data.js
+++ src/sections/Projects/data.js
@@ -2,11 +2,11 @@ import { SecuferLogo, SecuferDashboard } from '../../images/Secufer';
 import {
   friendsDriveImageSmall,
   friendsDriveImageLarge,
-  friendsDriveLogo,
+  friendsDriveLogo
 } from '../../images/FriendDrive';
 import {
   TowMeterSquareLogo,
-  TowMeterSquareMobile,
+  TowMeterSquareMobile
 } from '../../images/TowMeterSquare';
 import { FriendZone, FriendZoneLogo } from '../../images/FriendZone';
 import { Cinemagram, CinemagramLogo } from '../../images/Cinemagram';
@@ -17,7 +17,7 @@ const cinemagram = {
   logo: CinemagramLogo,
   images: {
     light: { small: Cinemagram, large: Cinemagram },
-    dark: { small: Cinemagram, large: Cinemagram },
+    dark: { small: Cinemagram, large: Cinemagram }
   },
   description:
     'Cinemagram Web app is Community Platform for Cinema buffs. A facebook/instagram-like inspired social media application.',
@@ -29,7 +29,7 @@ const cinemagram = {
     'Comment feature',
     'Profile Customization',
     'Followers/Following feature',
-    'Search Feature',
+    'Search Feature'
   ],
   techStack: [
     'React',
@@ -46,10 +46,10 @@ const cinemagram = {
     'Passport JS',
     'PostCSS',
     'Google Cloud Storage',
-    'Axios',
+    'Axios'
   ],
   link: 'https://cinemagram.prathmeshdhatrak.com/',
-  repo: 'https://github.com/Prathmesh-Dhatrak/cinemagram',
+  repo: 'https://github.com/Prathmesh-Dhatrak/cinemagram'
 };
 
 const secufer = {
@@ -58,7 +58,7 @@ const secufer = {
   logo: SecuferLogo,
   images: {
     light: { small: SecuferDashboard, large: SecuferDashboard },
-    dark: { small: SecuferDashboard, large: SecuferDashboard },
+    dark: { small: SecuferDashboard, large: SecuferDashboard }
   },
   description:
     'Secufer App is designed for a purpose that there should be a carefree environment while online shopping or transacting with someone who you dont trust much.',
@@ -68,11 +68,11 @@ const secufer = {
     'Creating Authenticated Transactions',
     'Creating Request Links',
     'Joining Transactions',
-    'Connecting Buyer & Seller',
+    'Connecting Buyer & Seller'
   ],
   techStack: ['React Native', 'REST API', 'WhatsApp Business API', 'Figma UI'],
   link: 'https://secuferapp.prathmeshdhatrak.com/',
-  repo: 'https://github.com/secufer/serufer-app',
+  repo: 'https://github.com/secufer/serufer-app'
 };
 
 const friendsZone = {
@@ -81,7 +81,7 @@ const friendsZone = {
   logo: FriendZoneLogo,
   images: {
     light: { small: FriendZone, large: FriendZone },
-    dark: { small: FriendZone, large: FriendZone },
+    dark: { small: FriendZone, large: FriendZone }
   },
   description:
     'A fun and easy to use Chat Room or workspace where friends can share resources and hang around.',
@@ -91,11 +91,11 @@ const friendsZone = {
     'Realtime group messaging',
     'Realtime resources sharing',
     'Supports Emoji',
-    'Reaction on messages',
+    'Reaction on messages'
   ],
   techStack: ['React JS', 'Firebase', 'Material UI', 'React Icons'],
   link: 'https://friends-zone.prathmeshdhatrak.com/',
-  repo: 'https://github.com/Prathmesh-Dhatrak/friend-zone',
+  repo: 'https://github.com/Prathmesh-Dhatrak/friend-zone'
 };
 
 const friendsDrive = {
@@ -104,7 +104,7 @@ const friendsDrive = {
   logo: friendsDriveLogo,
   images: {
     light: { small: friendsDriveImageSmall, large: friendsDriveImageLarge },
-    dark: { small: friendsDriveImageSmall, large: friendsDriveImageLarge },
+    dark: { small: friendsDriveImageSmall, large: friendsDriveImageLarge }
   },
   description: 'A fun and easy to use Drive space where you can store Photos.',
   features: [
@@ -112,10 +112,10 @@ const friendsDrive = {
     'Upload multiple Photos',
     'Create new Album',
     'Delete Photos / Albums',
-    'Download Photos',
+    'Download Photos'
   ],
   techStack: ['React JS', 'Firebase', 'Material UI', 'Redux'],
-  repo: 'https://github.com/Prathmesh-Dhatrak/friend-drive',
+  repo: 'https://github.com/Prathmesh-Dhatrak/friend-drive'
 };
 
 const towMeterSquare = {
@@ -124,7 +124,7 @@ const towMeterSquare = {
   logo: TowMeterSquareLogo,
   images: {
     light: { small: TowMeterSquareMobile, large: TowMeterSquareMobile },
-    dark: { small: TowMeterSquareMobile, large: TowMeterSquareMobile },
+    dark: { small: TowMeterSquareMobile, large: TowMeterSquareMobile }
   },
   description:
     'The Ecommerce website for TowMeterSquare Shop. Built with React.',
@@ -136,13 +136,13 @@ const towMeterSquare = {
     'Payment with Stripe',
     'Star Rating System',
     'Multiple Image Uploads',
-    'Products CRUD',
+    'Products CRUD'
   ],
   techStack: ['React JS', 'Redux', 'Firebase', 'MongoDB'],
   link: 'https://twometersquare.netlify.app/',
-  repo: 'https://github.com/Prathmesh-Dhatrak/client',
+  repo: 'https://github.com/Prathmesh-Dhatrak/client'
 };
 
-const data = [secufer, friendsZone, towMeterSquare, friendsDrive];
+const data = [cinemagram, secufer, friendsZone, towMeterSquare, friendsDrive];
 export default data;
 
