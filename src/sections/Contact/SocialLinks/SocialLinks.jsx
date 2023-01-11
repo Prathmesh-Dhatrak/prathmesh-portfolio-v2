dd35eb6062cf81ed711c89e993a6da8785012ced
Prathmesh Santosh Dhatrak <prathmesh101dhatrak@gmail.com>
2023-01-11 11:25:45 +0530

    Strings must use singlequote

--- src/sections/Contact/SocialLinks/SocialLinks.jsx
+++ src/sections/Contact/SocialLinks/SocialLinks.jsx
@@ -1,46 +1,46 @@
-import React from "react";
-import classes from "./social.module.scss";
+import React from 'react';
+import classes from './social.module.scss';
 
 export default function SocialLinks() {
   return (
     <div className={classes.social}>
       <a
-        href="mailto:prathmesh101dhatrak@gmail.com"
-        target={"_blank"}
-        className="icon"
-        rel="noreferrer"
+        href='mailto:prathmesh101dhatrak@gmail.com'
+        target={'_blank'}
+        className='icon'
+        rel='noreferrer'
       >
         &#xe910;
       </a>
       <a
-        href="https://github.com/Prathmesh-Dhatrak"
-        target={"_blank"}
-        className="icon"
-        rel="noreferrer"
+        href='https://github.com/Prathmesh-Dhatrak'
+        target={'_blank'}
+        className='icon'
+        rel='noreferrer'
       >
         &#xe908;
       </a>
       <a
-        href="https://twitter.com/Prathmesh007_SD"
-        target={"_blank"}
-        className="icon"
-        rel="noreferrer"
+        href='https://twitter.com/Prathmesh007_SD'
+        target={'_blank'}
+        className='icon'
+        rel='noreferrer'
       >
         &#xe912;
       </a>
       <a
-        href="https://www.linkedin.com/in/prathmeshsd/"
-        target={"_blank"}
-        className="icon"
-        rel="noreferrer"
+        href='https://www.linkedin.com/in/prathmeshsd/'
+        target={'_blank'}
+        className='icon'
+        rel='noreferrer'
       >
         &#xe90a;
       </a>
       <a
-        href="https://www.instagram.com/prathmesh007world/"
-        target={"_blank"}
-        className="icon"
-        rel="noreferrer"
+        href='https://www.instagram.com/prathmesh007world/'
+        target={'_blank'}
+        className='icon'
+        rel='noreferrer'
       >
         &#xe90e;
       </a>
